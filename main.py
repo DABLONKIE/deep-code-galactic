9988f95aaf39bb7aadebf8d64cc7bdbc124d6d51
DABLONKIE <94186808+DABLONKIE@users.noreply.github.com>
2022-09-26 22:37:44 +0200

    Making a class makes the screen go to black? wtf

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+tiles.set_current_tilemap(tilemap("""testingGround"""))
 
+#------------------Variables
+#player = Dwarf()
+isGrounded = False
+#------------------Starting Code
+
+
+
+#------------------Classes
+class Dwarf:
+    def __init__():
+        selfSprite = sprites.create(assets.image("""testPlayer"""), SpriteKind.player)
+    def update():
+        if selfSprite.tile_kind_at(TileDirection.BOTTOM, assets.tile("""rockFloor""")):
+            #if isGrounded == False:
+                #scene.camera_shake(2, 100)
+            isGrounded = True
+        if not isGrounded:
+            selfSprite.y += 3
+        selfSprite.x += controller.dx()  
+
+#------------------Main Game Loop
+while True:
+    #player.update()
+    pause(25)
+#------------------
--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
+tiles.setCurrentTilemap(tilemap`testingGround`)
+// ------------------Variables
+// player = Dwarf()
+let isGrounded = false
+// ------------------Starting Code
+// ------------------Classes
+class Dwarf {
+    
+    
+}
 
+// ------------------Main Game Loop
+while (true) {
+    // player.update()
+    pause(25)
+}
