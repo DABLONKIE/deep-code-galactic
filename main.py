000c57e03078386a9db32cc58a78a0723664779b
Blonc <94186808+DABLONKIE@users.noreply.github.com>
2022-09-29 23:38:44 +0200

    Movement and tileset changes

--- main.py
+++ main.py
@@ -1,27 +1,56 @@
 tiles.set_current_tilemap(tilemap("""testingGround"""))
 
 #------------------Variables
-#player = Dwarf()
-isGrounded = False
+#------------PlayerData
+playerSprite = sprites.create(assets.image("""testPlayer"""), SpriteKind.player)
+playerHealth = 100
+playerIsGrounded = False
+playerJumping = False
+playerGravity: number = 0
 #------------------Starting Code
+playerSprite.set_position(50, 50)
+scene.camera_follow_sprite(playerSprite)
 
-
-
-#------------------Classes
-class Dwarf:
-    def __init__():
-        selfSprite = sprites.create(assets.image("""testPlayer"""), SpriteKind.player)
-    def update():
-        if selfSprite.tile_kind_at(TileDirection.BOTTOM, assets.tile("""rockFloor""")):
-            #if isGrounded == False:
-                #scene.camera_shake(2, 100)
-            isGrounded = True
-        if not isGrounded:
-            selfSprite.y += 3
-        selfSprite.x += controller.dx()  
+#------------------Functions
+def PlayerJump():
+    global playerIsGrounded, playerGravity, playerJumping
+    print("jumpie")
+    if playerIsGrounded:
+        print("true!")
+        #playerJumping = True
+        playerGravity = -3.5
+        playerSprite.y += -3
 
 #------------------Main Game Loop
-while True:
-    #player.update()
-    pause(25)
+def PlayerLoop():
+    global playerHealth, playerIsGrounded, playerGravity
+
+    if playerSprite.tile_kind_at(TileDirection.BOTTOM, assets.tile("""rockFloor""")):
+        if not playerIsGrounded:
+            if playerGravity > 3:
+                music.thump.play()
+            scene.camera_shake(playerGravity / 3, 100)
+        playerIsGrounded = True
+    else:
+        playerIsGrounded = False
+
+    if not playerIsGrounded:
+        if playerGravity < 5:
+            playerGravity += 0.2 * Delta.DELTA()
+        playerSprite.vy += (playerGravity * Delta.DELTA()) * 15
+        playerSprite.vy *= 0.8
+    else:
+        playerGravity = 0
+    
+    playerSprite.vx += controller.dx() * 10
+    playerSprite.vx *= 0.8
+
+    if playerSprite.vx > 0.1:
+        playerSprite.set_image(assets.image("""testPlayer"""))
+    elif playerSprite.vx < -0.1:
+        playerSprite.set_image(assets.image("""testPlayerFlip"""))
+
+controller.player1.on_button_event(ControllerButton.A, ControllerButtonEvent.PRESSED, PlayerJump)
+forever(PlayerLoop)
+
 #------------------
--- main.ts
+++ main.ts
@@ -1,16 +1,60 @@
 tiles.setCurrentTilemap(tilemap`testingGround`)
 // ------------------Variables
-// player = Dwarf()
-let isGrounded = false
+// ------------PlayerData
+let playerSprite = sprites.create(assets.image`testPlayer`, SpriteKind.Player)
+let playerHealth = 100
+let playerIsGrounded = false
+let playerJumping = false
+let playerGravity = 0
 // ------------------Starting Code
-// ------------------Classes
-class Dwarf {
-    
-    
-}
-
+playerSprite.setPosition(50, 50)
+scene.cameraFollowSprite(playerSprite)
+// ------------------Functions
 // ------------------Main Game Loop
-while (true) {
-    // player.update()
-    pause(25)
-}
+controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function PlayerJump() {
+    
+    console.log("jumpie")
+    if (playerIsGrounded) {
+        console.log("true!")
+        // playerJumping = True
+        playerGravity = -3.5
+        playerSprite.y += -3
+    }
+    
+})
+forever(function PlayerLoop() {
+    
+    if (playerSprite.tileKindAt(TileDirection.Bottom, assets.tile`rockFloor`)) {
+        if (!playerIsGrounded) {
+            if (playerGravity > 3) {
+                music.thump.play()
+            }
+            
+            scene.cameraShake(playerGravity / 3, 100)
+        }
+        
+        playerIsGrounded = true
+    } else {
+        playerIsGrounded = false
+    }
+    
+    if (!playerIsGrounded) {
+        if (playerGravity < 5) {
+            playerGravity += 0.2 * Delta.DELTA()
+        }
+        
+        playerSprite.vy += playerGravity * Delta.DELTA() * 15
+        playerSprite.vy *= 0.8
+    } else {
+        playerGravity = 0
+    }
+    
+    playerSprite.vx += controller.dx() * 10
+    playerSprite.vx *= 0.8
+    if (playerSprite.vx > 0.1) {
+        playerSprite.setImage(assets.image`testPlayer`)
+    } else if (playerSprite.vx < -0.1) {
+        playerSprite.setImage(assets.image`testPlayerFlip`)
+    }
+    
+})
